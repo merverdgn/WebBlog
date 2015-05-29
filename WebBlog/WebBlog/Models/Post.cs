31723b66655870ec1ccb07b7fbc3ea2a25d606d4
Merve <merve.h.erdogan@gmail.com>
2015-05-29 11:31:12 +0300

    Create DB con. string, Create Post Model Class, Create Post Controller using post model

---
+++ WebBlog/WebBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebBlog.Models
+{
+   
+
+        public class Post
+        {
+            public int ID { get; set; }
+            public string TITLE { get; set; }
+            public string CONTENT { get; set; }
+            public string AUTHOR { get; set; }
+            public DateTime CREATEDATE { get; set; }
+        }
+
+        public class LocalDBContext : DbContext
+        {
+            public DbSet<Post> Posts { get; set; }
+        }
+
+
+    
+}
