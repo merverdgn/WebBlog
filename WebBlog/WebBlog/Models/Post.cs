0ee0b00f71c9f991ade7a58c8cd4b50f24427821
Merve <merve.h.erdogan@gmail.com>
2015-05-29 16:14:54 +0300

    İmport Template and other settings

--- WebBlog/WebBlog/Models/Post.cs
+++ WebBlog/WebBlog/Models/Post.cs
@@ -6,22 +6,22 @@ using System.Web;
 
 namespace WebBlog.Models
 {
-   
-
-        public class Post
-        {
-            public int ID { get; set; }
-            public string TITLE { get; set; }
-            public string CONTENT { get; set; }
-            public string AUTHOR { get; set; }
-            public DateTime CREATEDATE { get; set; }
-        }
-
-        public class LocalDBContext : DbContext
-        {
-            public DbSet<Post> Posts { get; set; }
-        }
 
 
-    
+    public class Post
+    {
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string CONTENT { get; set; }
+        public string AUTHOR { get; set; }
+        public DateTime CREATEDATE { get; set; }
+    }
+
+    public class LocalDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+
+
 }
