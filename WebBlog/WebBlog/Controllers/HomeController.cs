b68d2b1fe161a26cd45f157bfee949e2e8d2ffb9
Merve <merve.h.erdogan@gmail.com>
2015-05-29 17:12:55 +0300

    get posts gor home page

--- WebBlog/WebBlog/Controllers/HomeController.cs
+++ WebBlog/WebBlog/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBlog.Models;
 
 namespace WebBlog.Controllers
 {
     public class HomeController : Controller
     {
+
+        private LocalDBContext db = new LocalDBContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Posts.ToList());
         }
     }
 }
