059434a02392a30b1e8332ccb2af0e557293e59d
Merve <merve.h.erdogan@gmail.com>
2015-06-01 19:25:17 +0300

    add paging & some fix

--- WebBlog/WebBlog/Controllers/HomeController.cs
+++ WebBlog/WebBlog/Controllers/HomeController.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBlog.Models;
+using PagedList;
+
+
 
 namespace WebBlog.Controllers
 {
     public class HomeController : Controller
     {
 
+
         private LocalDBContext db = new LocalDBContext();
 
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View(db.Posts.ToList());
+           
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);            
+         
+            return View(db.Posts.ToList().OrderByDescending(e => e.CREATEDATE).ToList().ToPagedList(pageNumber, pageSize));
         }
 
     }
